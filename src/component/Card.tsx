87341b4ee8b0691db183d5086d4d80c5aeb7915d
michael jones <michael.jowen.jones@gmail.com>
2021-09-19 11:57:38 +0100

    formatted

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -2,18 +2,18 @@ interface Word {
   word: string;
   color: string;
   data: string[][];
-  setState(arr:string[][]):void;
+  setState(arr: string[][]): void;
 }
 
 export function Card({ word, color, data, setState }: Word): JSX.Element {
-
   const handleClick = () => {
-    const idx = data.findIndex(element => element[0] === word);
-    data[idx][1] = data[idx][1].length > 5 ? data[idx][1].slice(5) : data[idx][1];
+    const idx = data.findIndex((element) => element[0] === word);
+    data[idx][1] =
+      data[idx][1].length > 5 ? data[idx][1].slice(5) : data[idx][1];
     // console.log(idx,data[idx],word)x
     setState([...data]);
-  } 
-  
+  };
+
   return (
     <button className="card" id={color} onClick={handleClick}>
       {word}
