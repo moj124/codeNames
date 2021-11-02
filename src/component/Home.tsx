9d62a1ddd0e6bc9fa3dec7f4423b8acdf3646c9c
michael jones <michael.jowen.jones@gmail.com>
2021-11-02 13:49:34 +0000

    added a share link feature

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,8 +1,6 @@
 import styles from "./Home.module.css";
-// import {useState} from "react";
 import { BrowserRouter as Router, useHistory } from "react-router-dom";
 export function Home(): JSX.Element {
-  // const [viewable,setViewable] = useState<boolean>(true);
 
   const history = useHistory();
   const handleClick = async () => {
@@ -10,37 +8,12 @@ export function Home(): JSX.Element {
     const { session } = await res.json();
     history.push(`/game/${session}`);
   };
-
-  // const handleClickRules = () => {
-  //   setViewable(!viewable);
-  // };
-
   return (
     <>
       <header className={styles.home_header}>
         <div>
           <h1>CODENAMES</h1>
         </div>
-        {/* <div className={styles.content_head}>
-          <button className={styles.collapsible} onClick={handleClickRules}>Game Rules</button>
-          {viewable ? 
-          <div className={styles.viewable_content}>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
-              maxime dolore deleniti numquam accusamus voluptates, explicabo
-              aliquam architecto necessitatibus id itaque accusantium
-              consectetur a aut expedita, nemo iusto dicta esse.
-            </p>
-          </div>
-          :  <div className={styles.hide_content}>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
-            maxime dolore deleniti numquam accusamus voluptates, explicabo
-            aliquam architecto necessitatibus id itaque accusantium
-            consectetur a aut expedita, nemo iusto dicta esse.
-          </p>
-        </div>}
-        </div> */}
       </header>
       <main className={styles.main}>
         <Router>
